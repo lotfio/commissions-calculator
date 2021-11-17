cb75d62219a4c9e18e75cce5ec54be90debaa07d
lotfio <contact@lotfio.net>
2021-11-17 19:25:44 +0100

    use mocks

--- tests/Integration/Calculator.php
+++ tests/Integration/Calculator.php
@@ -15,17 +15,77 @@ declare(strict_types=1);
 namespace Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
 use CommissionsCalculator\Calculator;
 use CommissionsCalculator\Input;
 use CommissionsCalculator\Output;
 use CommissionsCalculator\Utility\Transactions;
 use CommissionsCalculator\Utility\Currency;
 use CommissionsCalculator\Utility\ProviderFactory;
-
 use CommissionsCalculator\Exceptions\TransactionsException;
+use CommissionsCalculator\Providers\BinLookupProvider;
+use CommissionsCalculator\Providers\ExchangeRateProvider;
+use DG\BypassFinals;
 
 class CalculatorTest extends TestCase
 {
+    public function setUp(): void
+    {
+        BypassFinals::enable();
+    }
+
+    private function getTransactionsMock(): MockObject
+    {
+        // mocking transactions object
+        $transactions = $this->getMockBuilder(Transactions::class)->getMock();
+        $transactions->method('parseFile')
+                    ->willReturn([  // transactions
+                        (object) [
+                            "bin"       => "45417360",
+                            "amount"    => "100.00",
+                            "currency"  => "AED"
+                        ],
+                        (object) [
+                            "bin"       => "45417360",
+                            "amount"    => "100.00",
+                            "currency"  => "ALL"
+                        ]
+                    ]);
+        return $transactions;
+    }
+
+    private function getProviderFactoryMock(): ProviderFactory
+    {
+        // mocking providerFactory object
+        $binProvider = $this->getMockBuilder(BinLookupProvider::class)->getMock();
+        $binProvider->method('provides')
+                    ->willReturn([ // card information
+                        "country" => [
+                            "alpha2" => "JP"
+                        ]
+                    ]);
+
+        $rateProvider = $this->getMockBuilder(ExchangeRateProvider::class)->getMock();
+        $rateProvider->method('provides')
+                    ->willReturn([ // exchange rates
+                          "AED" => '4.258097',
+                          "AFN" => '105.900773',
+                          "ALL" => '122.826026',
+                    ]);
+
+        // register mocked providers
+        $providerFactory    = new ProviderFactory;
+        $reflection         = new \ReflectionObject($providerFactory);
+        $property           = $reflection->getProperty('providers');
+        $property->setAccessible(true);
+        $property->setValue($providerFactory, [
+                "binLookup"     => $binProvider,
+                "exchangeRate"  => $rateProvider
+            ]);
+
+        return $providerFactory;
+    }
+
     public function testCalculatorInvalidTransactionsFile()
     {
         $calculator = new Calculator(
@@ -38,15 +98,17 @@ class CalculatorTest extends TestCase
 
     public function testCalculatorValidTransactionsFile()
     {
+        // output object
         $output = new Output;
         $output->enableTestMode();
+
         $calculator = new Calculator(
-            new Input(['','./tests/stubs/transactions.txt']), $output, new ProviderFactory, new Transactions, new Currency
+            new Input(['','']), $output,  $this->getProviderFactoryMock(), $this->getTransactionsMock(), new Currency
         );
 
         $result = explode("\n", $calculator->calculate());
 
         $this->assertIsArray($result);
-        $this->assertCount(6, $result); // 5 transactions with last \n white space
+        $this->assertCount(3, $result); // 2 transactions and last \n white space
     }
 }
